94c45d1f4327cdf7e96f53f3655401dfdea61442
mohammed <mabdelaal@ictcube.com>
2018-12-05 11:03:15 +0200

    add c, water tank and rect examples

---
+++ rect.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+width = int(input("Enter rectange width: "))
+length = int(input("Enter rectange length: "))
+
+area = width * length
+perimeter = (width + length) * 2
+
+print("Area = ", area)
+print("Perimeter = ", perimeter)
